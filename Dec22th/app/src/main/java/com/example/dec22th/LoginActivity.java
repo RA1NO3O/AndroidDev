d682682fe43d6e8c0dc94b1c7d8d86b6e5b576d4
RA1N <RA1NN1AR@users.noreply.github.com>
2019-12-22 16:33:56 +0800

    16：33

--- Dec22th/app/src/main/java/com/example/dec22th/LoginActivity.java
+++ Dec22th/app/src/main/java/com/example/dec22th/LoginActivity.java
@@ -5,16 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
-
+    public EditText txt_Phone;
+    public EditText txt_Password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        txt_Phone = findViewById(R.id.txt_Phone);
+        txt_Password = findViewById(R.id.txt_Password);
     }
     public void btnLogin_onClick(View v){
         Intent jumper = new Intent(this,MainActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("phone",txt_Phone.getText().toString());
+        jumper.putExtra("password" , txt_Password.getText().toString());
+        jumper.putExtras(bundle);
         startActivity(jumper);
     }
 }
--- Dec22th/app/src/main/java/com/example/dec22th/MainActivity.java
+++ Dec22th/app/src/main/java/com/example/dec22th/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.dec22th;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView lbl_Welcome;
+    private TextView lbl_Password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lbl_Welcome = findViewById(R.id.lbl_Welcome);
+        lbl_Password = findViewById(R.id.lbl_Password);
+        Bundle bundle = getIntent().getExtras();
+        String phone = (String) bundle.get("phone");
+        lbl_Welcome.setText("欢迎，"+phone);
+        lbl_Password.setText(("您的密码： "+getIntent().getStringExtra("password")));
     }
 }
