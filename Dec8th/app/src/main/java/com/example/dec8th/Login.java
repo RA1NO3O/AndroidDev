c3069ef52f1f79db0aa075ba63a9adaf36610648
RA1N <RA1NN1AR@users.noreply.github.com>
2019-12-29 15:54:45 +0800

    Dec29th 15:54

--- Dec8th/app/src/main/java/com/example/dec8th/Login.java
+++ Dec8th/app/src/main/java/com/example/dec8th/Login.java
@@ -1,10 +1,12 @@
 package com.example.dec8th;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Application;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
@@ -18,6 +20,20 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         txt_Phone=findViewById(R.id.txt_Phone);
         txt_Password=findViewById(R.id.txt_Password);
+
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar!=null){
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return  super.onOptionsItemSelected(item);
     }
     public void btnExit_onClick(View v){
         Intent jumper = new Intent(this,MainActivity.class);
